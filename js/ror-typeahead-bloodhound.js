53785c9351ec19cb0694ead54fda03a2104abe81
Amanda French <amanda@amandafrench.net>
2025-08-12 21:31:55 -0400

    make work

--- js/ror-typeahead-bloodhound.js
+++ js/ror-typeahead-bloodhound.js
@@ -29,8 +29,10 @@ $('#simple-api .typeahead').typeahead({
       suggestion: function (data) {
           var displayName = data.names.find(name => name.types.includes('ror_display'))?.value || '';
           var orgTypes = data.types?.map(type => type.charAt(0).toUpperCase() + type.slice(1)).join(', ') || '';
-          var countryName = data.locations?.geonames_details?.country_name || '';
-          return '<p><strong>' + displayName + '</strong><br>' + orgTypes + ', ' + countryName + '</p>';
+           var cityName = data.locations[0].geonames_details.name || '';
+		   var countryName = data.locations[0].geonames_details.country_name || '';
+          
+          return '<p>' + displayName + '<br><small>' + orgTypes + ' - ' + cityName + ', ' countryName + '<br><i>'+ altNames + '</i></small></p>';
       }
     },
     display: function (data) {
